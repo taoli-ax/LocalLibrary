a7a96cc2b6e3987b3370b0b7ed612cd93ef2e426
litao <li.tao@klotz.de>
2022-12-03 19:45:21 +0800

    add requirements.txt and and BookAdmin and BookInstanceAdmin and AuthorAdmin and add BookInline and BookInstanceInline

--- LocalLibrary/catalogs/admin.py
+++ LocalLibrary/catalogs/admin.py
@@ -1,8 +1,44 @@
 from django.contrib import admin
-from .models import Book,Language,Author,BookInstance,Genre
+from .models import Book, Language, Author, BookInstance, Genre
+
 # Register your models here.
-admin.site.register(Book)
+# admin.site.register(Book)
 admin.site.register(Language)
-admin.site.register(Author)
-admin.site.register(BookInstance)
+# admin.site.register(Author)
+# admin.site.register(BookInstance)
 admin.site.register(Genre)
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('author', 'title', 'display_genre', 'lan')
+    inlines = [BookInstanceInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('id','book','status','due_back')
+    list_filter = ('book','status','due_back')
+    fieldsets = (
+        ('info',{'fields':('book','id','imprint')}),
+        ('availability',{'fields':('status','due_back')})
+    )
+
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name','last_name',('date_of_birth','date_of_death')]
+    inlines = [BookInline]
+
+
+
