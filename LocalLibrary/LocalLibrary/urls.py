0e279720b75a8d571a80efae06be7e44b0def0ee
litao <li.tao@klotz.de>
2022-12-04 16:10:21 +0800

    add-login-logged_out-and-resetpassword-form-email-done-confirm-complete-templates-and-url

--- LocalLibrary/LocalLibrary/urls.py
+++ LocalLibrary/LocalLibrary/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalogs/',include('catalogs.urls')),
     path('',RedirectView.as_view(url='/catalogs/')),
+    path('accounts/',include('django.contrib.auth.urls')),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
